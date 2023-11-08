a184c4543a121bae973b91ca840ded7056944df8
Abdulbasit <aabdulrafiu721@gmail.com>
2023-11-08 18:02:30 +0100

    static typing in python

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''assigning data types to parameters and return values'''
+
+
+def add(a: float, b: float) -> float:
+    '''Adds two floating-point numbers'''
+    return a + b
