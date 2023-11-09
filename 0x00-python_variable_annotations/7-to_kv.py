dea319a6586001305bfd3c1e1f6981d2347c71a0
Abdulbasit <aabdulrafiu721@gmail.com>
2023-11-09 11:53:50 +0100

    done

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+'''returns a tuple of two parameters'''
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
+    '''k is a string and v is either int or float'''
+    return tuple((k, v))
