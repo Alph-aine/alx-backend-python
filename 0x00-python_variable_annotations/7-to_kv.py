da9bf201746a49612600a211375cb57d21e0a057
Abdulbasit <aabdulrafiu721@gmail.com>
2023-11-09 12:10:59 +0100

    done

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,4 +5,4 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''k is a string and v is either int or float'''
-    return (k, float(v))
+    return (k, float(v ** 2.0))
