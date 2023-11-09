e3c22e4945f580e32412adca3025ba3fe361306e
Abdulbasit <aabdulrafiu721@gmail.com>
2023-11-09 12:31:02 +0100

    done

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+''' Annotation is required for the function below'''
 
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''The types here are: Iterable, Sequence, List, Tuple and int'''
     return [(i, len(i)) for i in lst]
