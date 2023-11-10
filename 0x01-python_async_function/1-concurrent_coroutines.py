304d00a33e0c4f9ee5458653227345e16bd4ae76
Abdulbasit <aabdulrafiu721@gmail.com>
2023-11-10 07:48:03 +0100

    done

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Asynchronous routine that spawns wait_random
+n times with the specified max_delay
+'''
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    '''
+    n:  number of times to spawn
+    max_delay: Maximum delay in seconds
+    '''
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delay = await asyncio.gather(*tasks)
+    return delay
